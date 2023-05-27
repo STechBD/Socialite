faf04df15390fa617640548a72ce3f9f0500c7e8
Md. Ashraful Alam Shemul <aashemul@gmail.com>
2023-05-27 23:12:15 +0600

    Minor update

--- src/inc/stechbd-socialite.class.php
+++ src/inc/stechbd-socialite.class.php
@@ -4,13 +4,13 @@
 /**
  * Plugin Name: Socialite by STechBD.Net
  * Plugin URI: https://project.stechbd.net/socialite/
- * Description: Socialite is a powerful WordPress plugin that allows users to integrate social media login, sharing buttons, Facebook comments, and a messenger floating button into their WordPress websites. It provides seamless integration with popular social media platforms like Facebook, Twitter, and Google, making it easy for users to log in, share content, and engage with their audience.
+ * Description: Socialite is a powerful WordPress plugin that allows users to integrate social media login.
  * Version: 1.0.0
  * Version Code: 1
  * Author: S Technologies Limited
  * Author URI: https://www.stechbd.net
  * Created: May 26, 2023
- * Updated: May 26, 2023
+ * Updated: May 27, 2023
  */
 
 
--- src/stechbd-socialite.php
+++ src/stechbd-socialite.php
@@ -4,7 +4,7 @@
 /**
  * Plugin Name: Socialite by STechBD.Net
  * Plugin URI: https://project.stechbd.net/socialite/
- * Description: Socialite is a powerful WordPress plugin that allows users to integrate social media login, sharing buttons, Facebook comments, and a messenger floating button into their WordPress websites. It provides seamless integration with popular social media platforms like Facebook, Twitter, and Google, making it easy for users to log in, share content, and engage with their audience.
+ * Description: Socialite is a powerful WordPress plugin that allows users to integrate social media login.
  * Version: 1.0.0
  * Version Code: 1
  * Author: S Technologies Limited
@@ -13,7 +13,7 @@
  * Tested up to: 6.2.2
  * Requires PHP: 8.0
  * Created: May 23, 2023
- * Updated: May 26, 2023
+ * Updated: May 27, 2023
  * Text Domain: stechbd-socialite
  * Domain Path: /languages
  * License: MIT
